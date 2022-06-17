23a7796d8ea89b6e5b4d93a19b0a56cb6ee887bf
Baur Zhan <baur007@gmail.com>
2022-06-17 15:34:36 +0100

    rewritten the await/async version of getData function by promise version

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getData = async (baseURL) => {
-  const fetchedData = await axios.get(baseURL);
-  return fetchedData.data;
+export const getData = (baseURL) => {
+  return axios.get(baseURL)
+  .then(response => response.data)
 };
--- src/api/getDataCopy.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getDataCopy = async (baseURL, setFunc) => {
-  const fetchedData = await axios.get(baseURL);
-  setFunc(fetchedData.data);
-  
-};
